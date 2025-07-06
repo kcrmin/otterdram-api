b260bff2a8425e85a2744701c575b3184317a54c
kcrmin <kcrmin@gmail.com>
2025-07-06 23:58:48 +0900

    chore(boot): add Spring Boot project initialized via Spring Initializr

---
+++ src/main/java/com/otterdram/otterdram/OtterDramApplication.java
@@ -0,0 +1,14 @@
+package com.otterdram.otterdram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class OtterDramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtterDramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otterdram/otterdram/OtterDramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otterdram.otterdram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtterDramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
