98ecd8f24e9ea61470095a7b5e56c26bf84a4dd8
kcrmin <kcrmin@gmail.com>
2025-07-17 12:06:37 +0900

    chore(gitignore): update ignore rules to include enum files in target directory

---
+++ src/main/java/com/otterdram/otterdram/common/enums/target/CommentTargetEntity.java
@@ -0,0 +1,7 @@
+package com.otterdram.otterdram.common.enums.target;
+
+public enum CommentTargetEntity {
+    BRAND,
+    COLLECTION,
+    MODEL,
+}
---
+++ src/main/java/com/otterdram/otterdram/common/enums/target/TagTargetEntity.java
@@ -0,0 +1,10 @@
+package com.otterdram.otterdram.common.enums.target;
+
+public enum TagTargetEntity {
+    COMPANY,
+    DISTILLERY,
+    BRAND,
+    COLLECTION,
+    MODEL,
+    RELEASE,
+}
